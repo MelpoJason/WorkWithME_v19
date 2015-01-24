1f06f2fe0fb2b14c196732b9c552a5b3173dcd07
MelpoJason <jasonchan9280@gmail.com>
2015-01-24 23:06:38 +0800

    [basic data changed]redesigned the user basic info, and this data is for login the App.

--- WorkWithME_v19/src/com/workwithme/httpUtils/httpClient.java
+++ WorkWithME_v19/src/com/workwithme/httpUtils/httpClient.java
@@ -1,5 +1,11 @@
 package com.workwithme.httpUtils;
 
 public class httpClient {
+	private static String url = "";
+	private static int socketTimeout = 8000;
+	private static int connectionRetries = 3;
+	
+	
+	
 
 }
